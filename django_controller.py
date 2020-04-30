28105923aea885941dd3af175049383a88ebf4f9
Sakkeer Hussain <sakkeerhussainp@gmail.com>
2020-04-30 15:20:33 +0530

    updated django setup

--- django_controller.py
+++ django_controller.py
@@ -14,10 +14,10 @@ class DjangoController:
         with connection.cd(slug):
             connection.run('virtualenv venv -p $(which python3)')
             connection.run('source venv/bin/activate')
-            connection.run('ls')
-            connection.run('cd code && ls')
-            connection.run('pip install -r code/requirements.txt')
-            connection.run('pip freeze')
+        connection.sudo('apt-get -qq install libpq-dev python-dev')
+        connection.sudo('apt-get -qq install python-psycopg2')
+        connection.run('pip install -r {}/code/requirements.txt'.format(slug))
+        connection.run('pip freeze')
         # try:
         # except Exception as e:
         #     pass
